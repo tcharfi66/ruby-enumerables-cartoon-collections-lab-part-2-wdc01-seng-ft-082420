973f23e34608e0fdaaf0f672a1fb0a8e0dbf7ae5
Tej Charfi <charfi@usc.edu>
2020-08-17 22:32:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,7 +34,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
-  valid.find do |element|
+  valid_calls.find do |element|
     if planeteer_calls[element] == valid_calls[element]
               return element
     end
