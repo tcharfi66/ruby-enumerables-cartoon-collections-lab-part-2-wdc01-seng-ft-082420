e83df234c57eb1585e79a279d2b530328d21ffba
Tej Charfi <charfi@usc.edu>
2020-08-17 22:21:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   planeteer_calls.map do |element|
-    element.Capitalize
     element+= "!"
+    element.capitalize
     
   end
 end
